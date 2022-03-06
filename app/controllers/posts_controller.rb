6fb54ead6f6d91cedea887de6f7ceeeaf48abb9c
yukisuzuki0314 <bell.yuki19850314@icloud.com>
2022-03-06 14:38:01 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,14 @@
 class PostsController < ApplicationController
 
   def index
-    @posts = Post.all
+    @posts = Post.order(id: "DESC") # メモを降順、つまり、投稿の新しい順に表示
   end
 
-  def new
-  end
+  #def new
+  #end
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index # 追記
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("../memo")  // 追記　「..」は、「一つ上の階層」を意味
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
